f1af922e750e6e3f6c88a67d050326d7ab5afd1a
Chinzcode <chinzcoding@gmail.com>
2024-01-25 12:59:51 +0100

    fixed typo in dbh file

--- exercises/databaseExample/pdoAndDatabaseConn/includes/dbh.inc.php
+++ exercises/databaseExample/pdoAndDatabaseConn/includes/dbh.inc.php
@@ -1,12 +1,13 @@
 <?php
-
+//Connect to server
 $dns = "mysql;host=localhost;dbname=myfirstdatabase";
 $dbusername = "root";
 $dbpassword = "";
 
 try {
     $pdo = new PDO($dns, $dbusername, $dbpassword);
-    $pdo->setAttribute(PDO::ATTR_ERRORMODE, PDO::ERRMODE_EXCEPTION);
+    // set PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
