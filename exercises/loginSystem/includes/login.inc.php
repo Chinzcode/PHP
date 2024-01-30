c3710a66d88b94a713b750ece2d128b25792fc90
Trym <chinzcoding@gmail.com>
2024-01-30 16:41:23 +0100

    feat: implement basic login logic

--- exercises/loginSystem/includes/login.inc.php
+++ exercises/loginSystem/includes/login.inc.php
@@ -5,6 +5,37 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $pwd = $_POST["pwd"];  
 
     try {
+        require_once 'dbh.inc.php';
+        require_once 'login_model.inc.php';
+        require_once 'login_control.inc.php';
+
+        $errors = [];
+
+        if (is_input_empty($username, $pwd)) {
+            $errors["empty_input"] = "Fill in all fields!";
+        }
+
+        $result = get_user($pdo, $username);
+
+        if (is_username_wrong($result)) {
+            $errors["login_incorrect"] = "Incorrect login info!";
+        }
+
+        require_once 'config_session.inc.php';
+
+        if ($errors) {
+            $_SESSION["errors_signup"] = $errors;
+
+            $signupData = [
+                "username" => $username,
+                "email" => $email,
+            ];
+
+            $_SESSION["signup_data"] = $signupData;
+
+            header("location: ../index.php");
+            die();
+        }
 
     } catch (PDOException $e) {
         die("Query failed: " . $e->getMessage());
