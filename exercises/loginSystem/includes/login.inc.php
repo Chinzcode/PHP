c44965d86299184488c361469b23e4b2bbe34204
Trym <chinzcoding@gmail.com>
2024-01-31 16:03:49 +0100

    feat: enhance login system, add session handling, and add login_view.inc.php

--- exercises/loginSystem/includes/login.inc.php
+++ exercises/loginSystem/includes/login.inc.php
@@ -28,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         require_once 'config_session.inc.php';
 
         if ($errors) {
-            $_SESSION["errors_signup"] = $errors;
+            $_SESSION["errors_login"] = $errors;
 
 
             header("location: ../index.php");
@@ -38,7 +38,17 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $newSessionId = session_create_id();
         $sessionId = $newSessionId . "_" . $result["id"];
         session_id($sessionId);
-        
+
+        $_SESSION["user_id"] = $result["id"];
+        $_SESSION["user_username"] = htmlspecialchars($result["username"]);
+
+        $_SESSION["last_regeneration"] = time();
+
+        header("location: ../index.php?login=success");
+        $pdo = null;
+        $stmt = null;
+
+        die();
     } catch (PDOException $e) {
         die("Query failed: " . $e->getMessage());
     }
